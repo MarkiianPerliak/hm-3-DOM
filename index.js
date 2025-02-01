cec39455e24cca6a54a792aa50784eddc1e9c713
MarkiianPerliak <122627880+MarkiianPerliak@users.noreply.github.com>
2025-02-01 10:57:17 +0200

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+// 1
+const buttonElement = document.querySelector(".button1exe")
+const textElement = document.querySelector(".text1exe")
+buttonElement.textContent = "click me";
+// 2
+const imageElement = document.querySelector(".image2exe");
+imageElement.src = "./bruh.jpg";
+// 3
+const linkElement = document.querySelector(".link3exe")
+const imageElement2 = document.querySelector(".image3exe")
+imageElement2.alt = "hello!";
+linkElement.href = "https://edu.goiteens.com/learn/17035086/15266038/15266398/homework";
+// 4
+const itemElement = document.querySelector(".item4exe");
+const list = itemElement.parentNode;
+list.firstElementChild.textContent = "Done";
